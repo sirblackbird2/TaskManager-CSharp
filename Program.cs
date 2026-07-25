7274f44f86a60b59ac82d26a1c6ef3f1693e55d1
sirblackbird2 <sirblackblrd2@gmail.com>
2026-07-25 20:48:05 +0700

    Initial project setup

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using TaskManager.Services;
+TaskService taskService = new TaskService();
+
+string? readResult;
+string menuSelect = "";
+do
+{
+    Console.Clear();
+    Console.WriteLine("===== Task Manager =====");
+    Console.WriteLine("1. Add Task");
+    Console.WriteLine("2. View Tasks");
+    Console.WriteLine("3. Exit");
+    Console.WriteLine($"\nEnter an option: ");
+
+    readResult = Console.ReadLine();
+    Console.WriteLine();
+
+    if (readResult != null)
+    {
+        menuSelect = readResult.ToLower();
+    }
+
+    switch (menuSelect)
+    {
+        case "1":
+            Console.WriteLine("Enter task title: ");
+            string title = Console.ReadLine() ?? "";
+
+            Console.WriteLine("\nEnter task description: ");
+            string description = Console.ReadLine() ?? "";
+
+            taskService.AddTask(title, description);
+
+            Console.WriteLine("\nTask added successfully!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            break;
+
+        case "2":
+            taskService.DisplayTasks();
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            break;
+
+        case "3":
+            break;
+
+        default:
+            Console.WriteLine("Invalid option.");
+            Console.ReadKey();
+            break;
+    }
+} while (menuSelect != "3");
---
+++ Services/TaskService.cs
@@ -0,0 +1,62 @@
+using TaskManager.Models;
+namespace TaskManager.Services
+{
+    public class TaskService
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+        private int nextId = 1;
+        public void AddTask(string title, string description)
+        {
+            TaskItem task = new TaskItem();
+            task.Id = nextId;
+            nextId++;
+            task.Title = title;
+            task.Description = description;
+            task.IsCompleted = false;
+            tasks.Add(task);
+        }
+
+        public void DisplayTasks()
+        {
+            if (tasks.Count == 0)
+            {
+                Console.WriteLine("No tasks found");
+                return;
+            }
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"ID: {task.Id}");
+                Console.WriteLine($"Title: {task.Title}");
+                Console.WriteLine($"Description: {task.Description}");
+                Console.WriteLine($"Completed: {(task.IsCompleted ? "Yes" : "No")}");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine();
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            TaskItem? taskToDelete = null;
+            foreach (var task in tasks)
+            {
+                if (task.Id == id)
+                {
+                    taskToDelete = task;
+                    break;
+                }
+            }
+
+            if (taskToDelete != null)
+            {
+                tasks.Remove(taskToDelete);
+                Console.WriteLine("Task deleted successfully.");
+            }
+
+            else
+            {
+                Console.WriteLine("Task not found");
+            }
+        }
+    }
+}
